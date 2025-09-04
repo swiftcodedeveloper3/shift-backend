b3917ca50930fa3266d9712c79b813f049f21b65
arsalanmalik7 <arsalantech277@gmail.com>
2025-09-04 15:37:57 -0700

    admin login with cookie

--- src/controllers/adminController.mjs
+++ src/controllers/adminController.mjs
@@ -15,11 +15,25 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 export const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
+
+        if (!email || !password) return res.status(400).json({ message: 'Email and Password are required.' });
+
+        email = email.toLowerCase();
+
         const admin = await Admin.findOne({ email });
+
         if (!admin) return res.status(404).json({ message: 'Admin not found.' });
+
         const isMatch = await bcrypt.compare(password, admin.password);
+
         if (!isMatch) return res.status(401).json({ message: 'Invalid credentials.' });
+
         const token = jwt.sign({ id: admin._id, role: 'admin' }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', expiresIn: new Date(Date.now() + 86400000) });
+
+        
+
         res.status(200).json({ token, admin });
     } catch (err) {
         res.status(500).json({ message: 'Login failed', error: err.message });
