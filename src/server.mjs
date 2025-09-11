6b8c630ac2263f5ef4aa0a2b39de57a8c8d07319
marianoor09 <marianoor09@outlook.com>
2025-09-11 15:52:18 -0700

    path safe route

--- src/server.mjs
+++ src/server.mjs
@@ -79,7 +79,7 @@ app.get('/health', (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("/", (req, res) => {
+app.get((req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
