5b70cebfd53a7215ed657653815adac40c99c6b7
Tahir Shoukat <tahirshoukat@hotmail.com>
2025-12-21 21:14:42 +0500

    file too large (fixed)

--- src/config/fileUpload.mjs
+++ src/config/fileUpload.mjs
@@ -17,6 +17,11 @@ const storage = multer.diskStorage({
     },
 })
 
-const upload = multer({  storage: storage });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5 MB
+    }
+});
 
 export default upload;
--- src/server.mjs
+++ src/server.mjs
@@ -44,7 +44,9 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 app.use(cookieParser());
 
 app.use(morgan('dev'));
